eb7de60a3aac4b06ce5324da883be942765615c3
Maya Dahlke <30633474+mdalhlke@users.noreply.github.com>
2020-03-27 10:35:26 -0700

    update algo1

--- algo1.js
+++ algo1.js
@@ -3,14 +3,14 @@
  * @return {number}
  */
 var lengthOfLastWord = function(s) {
-    var wordCount = 0;
-    var reverse = s.trim().split('').reverse();
+    let wordCount = 0;
+    let reverse = s.trim().split('').reverse();
     
     if (s == '') {
         return 0;
     }
     
-    for (var char of reverse) {
+    for (let char of reverse) {
         if (char != ' '){
             wordCount++;
         }  else {
