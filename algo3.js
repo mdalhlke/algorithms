a5c9eaa2582d9c0307c9a1f59e1ed5dee770c233
Maya Dahlke <30633474+mdalhlke@users.noreply.github.com>
2020-04-20 20:06:09 -0700

    create algo3.js

---
+++ algo3.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    const bracketMatch = { 
+        '}': '{',
+        ')': '(', 
+        ']': '['
+    };
+    let openBracketArr = [];
+    
+    for (let c of s) {
+      let openBracket = bracketMatch[c];
+      if (openBracket) {
+        let lastOpenBracket = openBracketArr.shift();
+        if (lastOpenBracket !== openBracket){
+          return false;
+        }
+      } else {
+        openBracketArr.unshift(c);
+      }
+    }
+  
+    return openBracketArr.length === 0;
+};
